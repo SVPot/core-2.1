31cb7b915865533e75f54467bbf38f5e775b44f0
Sergey Potehin <spot7200@gmail.com>
2022-03-17 16:38:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
+
+        Iterator<Integer> iterator = intList.iterator();
+        while (iterator.hasNext()) {
+            Integer nextInt = iterator.next();
+            if (nextInt <= 0 || nextInt % 2 != 0) {
+                iterator.remove();
+            }
+        }
+//        Другой вариант удаления элементов по условию
+//        for (int i = 0; i < intList.size();) {
+//            if (intList.get(i) <= 0 || intList.get(i) % 2 != 0) {
+//                intList.remove(i);
+//            } else i++;
+//        }
+        Collections.sort(intList);
+        System.out.println(intList);
+    }
+}
